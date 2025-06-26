15d88d10ac610b5ea5613e6ec3b1bc399bcc4947
mattkosze <matthew.koszewicz@gmail.com>
2025-06-25 21:53:40 -0400

    P1N activation functions implemented

--- fnn/p1-numpy/classes.py
+++ fnn/p1-numpy/classes.py
@@ -12,3 +12,21 @@ class DenseLayer:
     def forward(self, inputs):
         # Calculate outputs using the method previously discussed
         self.outputs = np.dot(inputs, self.weights) + self.biases
+
+class ReLU:
+    # Our forward method; no overriding init method needed
+    def forward(self, inputs):
+        # Using the np.max() method to carry out our comparisons
+        self.output = np.maximum(0, inputs)
+
+
+class Softmax:
+    # Method for the forward pass; no init overriding needed
+    def forward(self, inputs):
+        # Calculate the non-normalized probabilities using np.exp(), which exponentiates everything, np.max() which finds the maximum value in the matrix, and np.sum() which performs a summation of each row/col as desired
+        unProb = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+
+        # Normalize the inputs for every sample
+        nProb = unProb / np.sum(unProb, axis=1, keepdims=True)
+
+        self.output = nProb
